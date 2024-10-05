e22b7298581d73ef57b43d68ca1e70e8a7f9cd93
JunMango <gudwns4985@gmail.com>
2024-10-05 21:28:16 +0900

    add portfolio template

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/soumyajiitttt/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://in.linkedin.com/in/soumyajiitttt"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/soumyajiitt"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./qualification.css";
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification 🧑🏻‍🎓</h2>
+      <span className="section__subtitle">My Personel Journey 🚗</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            Education
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">경희대학교</h3>
+                <span className="qualification__subtitle">컴퓨터 공학과</span>
+                <div className="qualification__calender">
+                  과 <i className="uil uil-calendar-alt"></i> 2020 - 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__round"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__round"></span>
+                <span className="qualification__line"></span>
+              </div>
+              {/*<div>*/}
+              {/*  <h3 className="qualification__title">Higher Secondary🧪</h3>*/}
+              {/*  <span className="qualification__subtitle">*/}
+              {/*    CBSE - Bengaluru*/}
+              {/*  </span>*/}
+              {/*  <div className="qualification__calender">*/}
+              {/*    <i className="uil uil-calendar-alt"></i> 2018 - 2020*/}
+              {/*  </div>*/}
+              {/*</div>*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
